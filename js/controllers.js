033d11ab4ff75fa442849c9f8eb7cc8848d422e0
Stavrus <stavrus@live.com>
2014-09-21 03:49:07 -0400

    Setup navigation and templates for other views.

--- js/controllers.js
+++ js/controllers.js
@@ -1,13 +1,52 @@
-var scrobblerApp = angular.module('scrobblerApp', []).config(['$compileProvider', function($compileProvider) {
+var scrobblerApp = angular.module('scrobblerApp', ['ui.router']).config(['$stateProvider', '$urlRouterProvider', '$compileProvider',
+    function($stateProvider, $urlRouterProvider, $compileProvider) {
         $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|app):/);
+        
+        $urlRouterProvider.otherwise('/');
+        $stateProvider.state('index', {
+          url: '/',
+          templateUrl: 'login.html',
+          controller: 'AppCtrl'
+        }).state('nowplaying', {
+          url: '/nowplaying',
+          templateUrl: 'nowplaying.html',
+          controller: 'NowPlayingCtrl'
+        }).state('profile', {
+          url: '/profile',
+          templateUrl: 'profile.html',
+          controller: 'ProfileCtrl'
+        }).state('settings', {
+          url: '/settings',
+          templateUrl: 'settings.html',
+          controller: 'SettingsCtrl'
+        }).state('statistics', {
+          url: '/statistics',
+          templateUrl: 'statistics.html',
+          controller: 'StatisticsCtrl'
+        });
     }
 ]);
 
-scrobblerApp.controller('AppCtrl', ['$scope', function ($scope) {  
+scrobblerApp.controller('AppCtrl', ['$scope', '$state', function ($scope, $state) {
   $scope.user = {name: '', password: ''};
     
+  $scope.toggleOffCanvas = function()
+  {
+      $('#wrapper').toggleClass('move-right');
+  };
+  
+  $scope.logOff = function()
+  {
+    $('#navbar').toggle();
+    $('#wrapper').removeClass('move-right');
+    $state.go('index');
+  }
+  
   $scope.login = function(e)
   {
+    $('#navbar').toggle();
+    $state.go('profile');
+    return;
     var apiKey = "10379ce465238e42fbc1a7aef57b49cd";
     var apiSecret = "4db90ca38c72a8b2e855ba19f1328f0d";
 
@@ -56,4 +95,20 @@ scrobblerApp.controller('AppCtrl', ['$scope', function ($scope) {
     //If we fail to write out, toast to the user
     request.onerror=function(){console.log("Error writing settings, did you give permission?");console.log(this.error);}  
   };
+}]);
+
+scrobblerApp.controller('NowPlayingCtrl', ['$scope', function ($scope) {  
+  
+}]);
+
+scrobblerApp.controller('ProfileCtrl', ['$scope', function ($scope) {  
+  
+}]);
+
+scrobblerApp.controller('SettingsCtrl', ['$scope', function ($scope) {  
+  
+}]);
+
+scrobblerApp.controller('StatisticsCtrl', ['$scope', function ($scope) {  
+  
 }]);
